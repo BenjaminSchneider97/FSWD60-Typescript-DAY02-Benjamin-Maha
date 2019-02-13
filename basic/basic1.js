a99232a87ee36097cd47d85c1516e19b9780d213
Benjamin Schneider <benjamin.schneider.97@gmail.com>
2019-02-13 09:50:57 +0100

    Finish basic 1

---
+++ basic/basic1.js
@@ -0,0 +1,18 @@
+var person = /** @class */ (function () {
+    function person(name, age, JobTitle) {
+        this.name = "";
+        this.age = "";
+        this.JobTitle = "";
+        this.name = name;
+        this.age = age;
+        this.JobTitle = JobTitle;
+    }
+    person.prototype.string = function () {
+        return "Hello there, My name is " + this.name + " and I am " + this.age + " years old, and I am a " + this.JobTitle;
+    };
+    return person;
+}());
+var fullstring = new person("Maha", "20", "Developer");
+var fullstring2 = new person("Ben", "19", "Developer");
+console.log(fullstring.string());
+console.log(fullstring2.string());
---
+++ basic/basic1.ts
@@ -0,0 +1,19 @@
+class person{
+	name = "";
+	age = "";
+	JobTitle = "";
+
+	constructor(name, age, JobTitle){
+		this.name = name;
+		this.age = age;
+		this.JobTitle = JobTitle;
+	}
+	string(){
+		return `Hello there, My name is ${this.name} and I am ${this.age} years old, and I am a ${this.JobTitle}`
+	}
+}
+
+var fullstring = new person("Maha", "20", "Developer");
+var fullstring2 = new person("Ben", "19", "Developer");
+console.log(fullstring.string());
+console.log(fullstring2.string());
